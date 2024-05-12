e744b6b1cb6d2e6ce5c58d2a1c9e229a01b73680
Khalilo1902 <131873932+Khalilo1902@users.noreply.github.com>
2024-05-12 14:44:39 +0200

    Mohamed (#5)
    * installation pnpm
    
    * Update
    
    ---------
    
    Co-authored-by: MedLaaribi <mohamed.laaribi.dev@gmail.com>

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import Carousel from "../components/carousel/Carousel";
-
+import ImageCard from "../components/imagecard/ImageCard";
+import AboutUsSection from "../components/aboutussection/AboutUsSection";
 const HomePage = () => {
   const slides: string[] = [
     "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
@@ -7,7 +8,40 @@ const HomePage = () => {
     "https://wallpaperaccess.com/full/809523.jpg",
     "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
   ];
+
+  const images = [
+    {
+      src: 'https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+      title: 'Europarat ',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1494145904049-0dca59b4bbad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
+      title: 'EDF',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1502675135487-e971002a6adb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
+      title: 'UNSER AKTION',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.',
+    },
+  ];
+
   return (
+
+      <div className="container">
+        <div className="w-1/2 m-auto pt-8">
+          <Carousel slides={slides}/>
+        </div>
+        <div className="container">
+          <AboutUsSection/>
+        </div>
+        <div className="flex min-h-screen items-center justify-center bg-neutral-800">
+          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+            {images.map((image, index) => (
+                <ImageCard key={index} {...image} />
+            ))}
+
     <div className=" container mx-auto ">
        <div className="md:w-1/2  max-w-full mx-3 md:m-auto pt-8 ">
         <Carousel slides={slides} />
@@ -45,10 +79,11 @@ const HomePage = () => {
             <div className=" w-full flex justify-end">
               <img className=" " src="/biQuoteUnten.png" alt="" />
             </div>
+
           </div>
+
         </div>
       </div>
-    </div>
   );
 };
 
